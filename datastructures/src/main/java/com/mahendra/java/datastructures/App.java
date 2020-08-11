cbf1a07656c59bfdd9e105fc6f5c18d12de58891
Mahendra <mahendra.cse.iiit@gmail.com>
2020-08-11 22:02:32 +0530

    created a maven project quickstart archtype and added to git repository

---
+++ datastructures/src/main/java/com/mahendra/java/datastructures/App.java
@@ -0,0 +1,13 @@
+package com.mahendra.java.datastructures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
